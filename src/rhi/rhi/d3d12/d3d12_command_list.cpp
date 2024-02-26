535db1020622bd3fa8d24e8364eb4f94525b9eeb
Robert Ryan <33040161+rtryan98@users.noreply.github.com>
2024-02-26 20:07:00 +0100

    command list skeleton

---
+++ src/rhi/rhi/d3d12/d3d12_command_list.cpp
@@ -0,0 +1,8 @@
+#pragma once
+
+#include "rhi/d3d12/d3d12_command_list.hpp"
+
+namespace rhi::d3d12
+{
+}
+
--- src/rhi/rhi/d3d12/d3d12_command_list.hpp
+++ src/rhi/rhi/d3d12/d3d12_command_list.hpp
@@ -3,11 +3,49 @@
 
 #include "rhi/command_list.hpp"
 
+#include <agility_sdk/d3d12.h>
+
 namespace rhi::d3d12
 {
 class D3D12_Command_List final : public Command_List
 {
+public:
+    // Barrier commands
+    virtual void barrier() noexcept = 0;
 
+    // Compute commands
+    virtual void dispatch(uint32_t groups_x, uint32_t groups_y, uint32_t groups_z) noexcept = 0;
+
+    // Copy commands
+    virtual void copy_buffer(Buffer* src, Buffer* dst, uint32_t size, uint32_t offset) noexcept = 0;
+    virtual void copy_buffer_to_image() noexcept = 0;
+    virtual void copy_image() noexcept = 0;
+    virtual void copy_image_to_buffer() noexcept = 0;
+
+    // Debug commands
+    virtual void begin_debug_region() noexcept = 0;
+    virtual void add_debug_marker()  noexcept = 0;
+    virtual void end_debug_region() noexcept = 0;
+
+    // Draw commands
+    virtual void draw(uint32_t vertex_count, uint32_t instance_count, uint32_t vertex_offset, uint32_t instance_offset) noexcept = 0;
+    virtual void draw_indirect() noexcept = 0;
+    virtual void draw_indirect_count() noexcept = 0;
+    virtual void draw_indexed(uint32_t index_count, uint32_t instance_count, uint32_t index_offset, uint32_t vertex_offset, uint32_t instance_offset) noexcept = 0;
+    virtual void draw_indexed_indirect() noexcept = 0;
+    virtual void draw_indexed_indirect_count() noexcept = 0;
+    virtual void draw_mesh_tasks(uint32_t groups_x, uint32_t groups_y, uint32_t groups_z) noexcept = 0;
+
+    // State commands
+    virtual void begin_render_pass() noexcept = 0;
+    virtual void end_render_pass() noexcept = 0;
+    virtual void set_pipeline(Pipeline* pipeline) noexcept = 0;
+    virtual void set_push_constants(void* data, uint32_t size) noexcept = 0;
+    virtual void set_scissor(int32_t x, int32_t y, uint32_t width, uint32_t height) noexcept = 0;
+    virtual void set_viewport(float x, float y, float width, float height, float min_depth, float max_depth, [[maybe_unused]] bool vk_invert_viewport = true) noexcept = 0;
+
+private:
+    ID3D12GraphicsCommandList7* m_cmd;
 };
 }
 
