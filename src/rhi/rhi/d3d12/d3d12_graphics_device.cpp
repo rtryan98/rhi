3439b860f861060c460e9b94e6fd92c0e23b35e6
Robert Ryan <33040161+rtryan98@users.noreply.github.com>
2025-02-20 17:41:19 +0100

    move context to rhi

--- src/rhi/rhi/d3d12/d3d12_graphics_device.cpp
+++ src/rhi/rhi/d3d12/d3d12_graphics_device.cpp
@@ -13,6 +13,211 @@ namespace rhi::d3d12
 {
 constexpr static uint32_t MAX_RTV_DSV_DESCRIPTORS = 1024;
 
+HRESULT create_d3d12_command_queue(
+    ID3D12Device* device,
+    D3D12_COMMAND_LIST_TYPE type,
+    bool disable_tdr,
+    ID3D12CommandQueue** queue)
+{
+    D3D12_COMMAND_QUEUE_DESC desc = {
+        .Type = type,
+        .Priority = D3D12_COMMAND_QUEUE_PRIORITY_NORMAL,
+        .Flags = disable_tdr
+            ? D3D12_COMMAND_QUEUE_FLAG_DISABLE_GPU_TIMEOUT
+            : D3D12_COMMAND_QUEUE_FLAG_NONE,
+        .NodeMask = 0
+    };
+    return device->CreateCommandQueue(&desc, IID_PPV_ARGS(queue));
+}
+
+void get_d3d12_features(D3D12_Context* context)
+{
+    context->features = {};
+    auto check_feature = [context](D3D12_FEATURE feature, auto& feature_data) {
+        context->device->CheckFeatureSupport(feature, &feature_data, sizeof(feature_data));
+        };
+    check_feature(D3D12_FEATURE_FEATURE_LEVELS, context->features.levels);
+    check_feature(D3D12_FEATURE_GPU_VIRTUAL_ADDRESS_SUPPORT, context->features.gpu_virtual_address_support);
+    check_feature(D3D12_FEATURE_SHADER_MODEL, context->features.shader_model);
+    check_feature(D3D12_FEATURE_PROTECTED_RESOURCE_SESSION_SUPPORT, context->features.protected_resource_session_support);
+    check_feature(D3D12_FEATURE_ROOT_SIGNATURE, context->features.root_signature);
+    check_feature(D3D12_FEATURE_ARCHITECTURE1, context->features.architecture1);
+    check_feature(D3D12_FEATURE_SHADER_CACHE, context->features.shader_cache);
+    check_feature(D3D12_FEATURE_EXISTING_HEAPS, context->features.existing_heaps);
+    check_feature(D3D12_FEATURE_SERIALIZATION, context->features.serialization);
+    check_feature(D3D12_FEATURE_DISPLAYABLE, context->features.displayable);
+    check_feature(D3D12_FEATURE_PROTECTED_RESOURCE_SESSION_TYPE_COUNT, context->features.protected_resource_session_type_count);
+    check_feature(D3D12_FEATURE_PROTECTED_RESOURCE_SESSION_TYPES, context->features.protected_resource_session_types);
+    check_feature(D3D12_FEATURE_D3D12_OPTIONS, context->features.options);
+    check_feature(D3D12_FEATURE_D3D12_OPTIONS1, context->features.options1);
+    check_feature(D3D12_FEATURE_D3D12_OPTIONS2, context->features.options2);
+    check_feature(D3D12_FEATURE_D3D12_OPTIONS3, context->features.options3);
+    check_feature(D3D12_FEATURE_D3D12_OPTIONS4, context->features.options4);
+    check_feature(D3D12_FEATURE_D3D12_OPTIONS5, context->features.options5);
+    check_feature(D3D12_FEATURE_D3D12_OPTIONS6, context->features.options6);
+    check_feature(D3D12_FEATURE_D3D12_OPTIONS7, context->features.options7);
+    check_feature(D3D12_FEATURE_D3D12_OPTIONS8, context->features.options8);
+    check_feature(D3D12_FEATURE_D3D12_OPTIONS9, context->features.options9);
+    check_feature(D3D12_FEATURE_D3D12_OPTIONS10, context->features.options10);
+    check_feature(D3D12_FEATURE_D3D12_OPTIONS11, context->features.options11);
+    check_feature(D3D12_FEATURE_D3D12_OPTIONS12, context->features.options12);
+    check_feature(D3D12_FEATURE_D3D12_OPTIONS13, context->features.options13);
+    check_feature(D3D12_FEATURE_D3D12_OPTIONS14, context->features.options14);
+    check_feature(D3D12_FEATURE_D3D12_OPTIONS15, context->features.options15);
+    check_feature(D3D12_FEATURE_D3D12_OPTIONS16, context->features.options16);
+    check_feature(D3D12_FEATURE_D3D12_OPTIONS17, context->features.options17);
+    check_feature(D3D12_FEATURE_D3D12_OPTIONS18, context->features.options18);
+    check_feature(D3D12_FEATURE_D3D12_OPTIONS19, context->features.options19);
+    check_feature(D3D12_FEATURE_D3D12_OPTIONS20, context->features.options20);
+}
+
+HRESULT create_d3d12_context(const D3D12_Context_Create_Info& create_info, D3D12_Context* context)
+{
+    HRESULT result = S_OK;
+    uint32_t factory_flags = 0;
+    if (create_info.enable_validation)
+    {
+        factory_flags |= DXGI_CREATE_FACTORY_DEBUG;
+        ID3D12Debug6* debug = nullptr;
+        if (SUCCEEDED(D3D12GetDebugInterface(IID_PPV_ARGS(&debug))))
+        {
+            debug->EnableDebugLayer();
+            if (create_info.enable_gpu_validation)
+            {
+                debug->SetEnableGPUBasedValidation(true);
+            }
+            debug->Release();
+        }
+    }
+    result = CreateDXGIFactory2(factory_flags, IID_PPV_ARGS(&context->factory));
+    if (FAILED(result))
+    {
+        return result;
+    }
+    result = context->factory->EnumAdapterByGpuPreference(0, DXGI_GPU_PREFERENCE_HIGH_PERFORMANCE, IID_PPV_ARGS(&context->adapter));
+    if (FAILED(result))
+    {
+        return result;
+    }
+    result = D3D12CreateDevice(context->adapter, create_info.feature_level, IID_PPV_ARGS(&context->device));
+    if (FAILED(result))
+    {
+        return result;
+    }
+    get_d3d12_features(context);
+    result = create_d3d12_command_queue(context->device, D3D12_COMMAND_LIST_TYPE_DIRECT, create_info.disable_tdr, &context->direct_queue);
+    if (FAILED(result))
+    {
+        return result;
+    }
+    result = create_d3d12_command_queue(context->device, D3D12_COMMAND_LIST_TYPE_COMPUTE, create_info.disable_tdr, &context->compute_queue);
+    if (FAILED(result))
+    {
+        return result;
+    }
+    result = create_d3d12_command_queue(context->device, D3D12_COMMAND_LIST_TYPE_COPY, create_info.disable_tdr, &context->copy_queue);
+    if (FAILED(result))
+    {
+        return result;
+    }
+    D3D12_DESCRIPTOR_HEAP_DESC descriptor_heap_desc = {
+        .Type = D3D12_DESCRIPTOR_HEAP_TYPE_CBV_SRV_UAV,
+        .NumDescriptors = create_info.resource_descriptor_heap_size,
+        .Flags = D3D12_DESCRIPTOR_HEAP_FLAG_SHADER_VISIBLE,
+        .NodeMask = 0
+    };
+    result = context->device->CreateDescriptorHeap(&descriptor_heap_desc, IID_PPV_ARGS(&context->resource_descriptor_heap));
+    if (FAILED(result))
+    {
+        return result;
+    }
+
+    descriptor_heap_desc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_SAMPLER;
+    descriptor_heap_desc.NumDescriptors = create_info.sampler_descriptor_heap_size;
+    result = context->device->CreateDescriptorHeap(&descriptor_heap_desc, IID_PPV_ARGS(&context->sampler_descriptor_heap));
+    if (FAILED(result))
+    {
+        return result;
+    }
+
+    descriptor_heap_desc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_RTV;
+    descriptor_heap_desc.NumDescriptors = create_info.rtv_descriptor_heap_size;
+    descriptor_heap_desc.Flags = D3D12_DESCRIPTOR_HEAP_FLAG_NONE;
+    result = context->device->CreateDescriptorHeap(&descriptor_heap_desc, IID_PPV_ARGS(&context->rtv_descriptor_heap));
+    if (FAILED(result))
+    {
+        return result;
+    }
+
+    descriptor_heap_desc.Type = D3D12_DESCRIPTOR_HEAP_TYPE_DSV;
+    descriptor_heap_desc.NumDescriptors = create_info.dsv_descriptor_heap_size;
+    result = context->device->CreateDescriptorHeap(&descriptor_heap_desc, IID_PPV_ARGS(&context->dsv_descriptor_heap));
+    if (FAILED(result))
+    {
+        return result;
+    }
+
+    D3D12_ROOT_PARAMETER1 push_constants_root_para = {
+        .ParameterType = D3D12_ROOT_PARAMETER_TYPE_32BIT_CONSTANTS,
+        .Constants = {
+            .ShaderRegister = 0,
+            .RegisterSpace = 0,
+            .Num32BitValues = create_info.push_constant_size >> 2
+        },
+        .ShaderVisibility = D3D12_SHADER_VISIBILITY_ALL
+    };
+    D3D12_VERSIONED_ROOT_SIGNATURE_DESC versioned_root_signature_desc = {
+        .Version = D3D_ROOT_SIGNATURE_VERSION_1_1,
+        .Desc_1_1 = {
+            .NumParameters = 1,
+            .pParameters = &push_constants_root_para,
+            .NumStaticSamplers = uint32_t(create_info.static_samplers.size()),
+            .pStaticSamplers = create_info.static_samplers.data(),
+            .Flags = D3D12_ROOT_SIGNATURE_FLAG_CBV_SRV_UAV_HEAP_DIRECTLY_INDEXED
+                | D3D12_ROOT_SIGNATURE_FLAG_SAMPLER_HEAP_DIRECTLY_INDEXED
+        }
+    };
+    ID3DBlob* root_signature_blob;
+    result = D3D12SerializeVersionedRootSignature(&versioned_root_signature_desc, &root_signature_blob, nullptr);
+    if (FAILED(result))
+    {
+        return result;
+    }
+    result = context->device->CreateRootSignature(0,
+        root_signature_blob->GetBufferPointer(),
+        root_signature_blob->GetBufferSize(),
+        IID_PPV_ARGS(&context->bindless_root_signature));
+    root_signature_blob->Release();
+    if (FAILED(result))
+    {
+        return result;
+    }
+
+    return S_OK;
+}
+
+HRESULT destroy_d3d12_context(D3D12_Context* context)
+{
+    context->bindless_root_signature->Release();
+    context->dsv_descriptor_heap->Release();
+    context->rtv_descriptor_heap->Release();
+    context->sampler_descriptor_heap->Release();
+    context->resource_descriptor_heap->Release();
+    context->copy_queue->Release();
+    context->compute_queue->Release();
+    context->direct_queue->Release();
+    ID3D12DebugDevice* debug_device;
+    bool has_debug_device = SUCCEEDED(context->device->QueryInterface(IID_PPV_ARGS(&debug_device)));
+    if (has_debug_device)
+    {
+        debug_device->ReportLiveDeviceObjects(D3D12_RLDO_DETAIL | D3D12_RLDO_IGNORE_INTERNAL);
+    }
+    context->device->Release();
+    context->adapter->Release();
+    context->factory->Release();
+    return S_OK;
+}
+
 DWORD await_fence(ID3D12Fence* fence, uint64_t val, uint64_t timeout)
 {
     DWORD result = WAIT_FAILED;
@@ -47,7 +252,7 @@ DWORD await_queue(ID3D12Device* device, ID3D12CommandQueue* queue, uint64_t time
     return result;
 }
 
-DWORD await_context(core::d3d12::D3D12_Context* context)
+DWORD await_context(D3D12_Context* context)
 {
     if (auto result = await_queue(context->device, context->direct_queue, INFINITE)) return result;
     if (auto result = await_queue(context->device, context->compute_queue, INFINITE)) return result;
@@ -138,7 +343,7 @@ D3D12_Graphics_Device::D3D12_Graphics_Device(const Graphics_Device_Create_Info& 
     , m_rtv_descriptor_indices()
     , m_dsv_descriptor_indices()
 {
-    core::d3d12::D3D12_Context_Create_Info context_create_info = {
+    D3D12_Context_Create_Info context_create_info = {
         .enable_validation = create_info.enable_validation,
         .enable_gpu_validation = create_info.enable_gpu_validation,
         .disable_tdr = false,
@@ -150,7 +355,7 @@ D3D12_Graphics_Device::D3D12_Graphics_Device(const Graphics_Device_Create_Info& 
         .push_constant_size = PUSH_CONSTANT_MAX_SIZE,
         .static_samplers = {}
     };
-    core::d3d12::create_d3d12_context(context_create_info, &m_context);
+    create_d3d12_context(context_create_info, &m_context);
     D3D12MA::ALLOCATOR_DESC allocator_desc = {
         .Flags = D3D12MA::ALLOCATOR_FLAG_SINGLETHREADED,
         .pDevice = m_context.device,
@@ -230,7 +435,7 @@ D3D12_Graphics_Device::~D3D12_Graphics_Device() noexcept
     m_indirect_signatures.draw_mesh_tasks_indirect->Release();
     m_indirect_signatures.dispatch_indirect->Release();
     m_allocator->Release();
-    core::d3d12::destroy_d3d12_context(&m_context);
+    destroy_d3d12_context(&m_context);
 }
 
 Result D3D12_Graphics_Device::wait_idle() noexcept
@@ -1489,7 +1694,7 @@ Indirect_Signatures D3D12_Graphics_Device::create_execute_indirect_signatures() 
     return result;
 }
 
-core::d3d12::D3D12_Context* D3D12_Graphics_Device::get_context() noexcept
+D3D12_Context* D3D12_Graphics_Device::get_context() noexcept
 {
     return &m_context;
 }
--- src/rhi/rhi/d3d12/d3d12_graphics_device.hpp
+++ src/rhi/rhi/d3d12/d3d12_graphics_device.hpp
@@ -30,6 +30,74 @@ struct Indirect_Signatures
     ID3D12CommandSignature* dispatch_indirect;
 };
 
+struct D3D12_Features
+{
+    // D3D12_FEATURE_DATA_ARCHITECTURE architecture; // Superseeded by architecture1
+    D3D12_FEATURE_DATA_FEATURE_LEVELS levels;
+    D3D12_FEATURE_DATA_GPU_VIRTUAL_ADDRESS_SUPPORT gpu_virtual_address_support;
+    D3D12_FEATURE_DATA_SHADER_MODEL shader_model;
+    D3D12_FEATURE_DATA_PROTECTED_RESOURCE_SESSION_SUPPORT protected_resource_session_support;
+    D3D12_FEATURE_DATA_ROOT_SIGNATURE root_signature;
+    D3D12_FEATURE_DATA_ARCHITECTURE1 architecture1;
+    D3D12_FEATURE_DATA_SHADER_CACHE shader_cache;
+    D3D12_FEATURE_DATA_EXISTING_HEAPS existing_heaps;
+    D3D12_FEATURE_DATA_SERIALIZATION serialization;
+    D3D12_FEATURE_DATA_DISPLAYABLE displayable;
+    D3D12_FEATURE_DATA_PROTECTED_RESOURCE_SESSION_TYPE_COUNT protected_resource_session_type_count;
+    D3D12_FEATURE_DATA_PROTECTED_RESOURCE_SESSION_TYPES protected_resource_session_types;
+    D3D12_FEATURE_DATA_D3D12_OPTIONS   options;
+    D3D12_FEATURE_DATA_D3D12_OPTIONS1  options1;
+    D3D12_FEATURE_DATA_D3D12_OPTIONS2  options2;
+    D3D12_FEATURE_DATA_D3D12_OPTIONS3  options3;
+    D3D12_FEATURE_DATA_D3D12_OPTIONS4  options4;
+    D3D12_FEATURE_DATA_D3D12_OPTIONS5  options5;
+    D3D12_FEATURE_DATA_D3D12_OPTIONS6  options6;
+    D3D12_FEATURE_DATA_D3D12_OPTIONS7  options7;
+    D3D12_FEATURE_DATA_D3D12_OPTIONS8  options8;
+    D3D12_FEATURE_DATA_D3D12_OPTIONS9  options9;
+    D3D12_FEATURE_DATA_D3D12_OPTIONS10 options10;
+    D3D12_FEATURE_DATA_D3D12_OPTIONS11 options11;
+    D3D12_FEATURE_DATA_D3D12_OPTIONS12 options12;
+    D3D12_FEATURE_DATA_D3D12_OPTIONS13 options13;
+    D3D12_FEATURE_DATA_D3D12_OPTIONS14 options14;
+    D3D12_FEATURE_DATA_D3D12_OPTIONS15 options15;
+    D3D12_FEATURE_DATA_D3D12_OPTIONS16 options16;
+    D3D12_FEATURE_DATA_D3D12_OPTIONS17 options17;
+    D3D12_FEATURE_DATA_D3D12_OPTIONS18 options18;
+    D3D12_FEATURE_DATA_D3D12_OPTIONS19 options19;
+    D3D12_FEATURE_DATA_D3D12_OPTIONS20 options20;
+};
+
+struct D3D12_Context_Create_Info
+{
+    bool enable_validation;
+    bool enable_gpu_validation;
+    bool disable_tdr;
+    D3D_FEATURE_LEVEL feature_level;
+    uint32_t resource_descriptor_heap_size;
+    uint32_t sampler_descriptor_heap_size;
+    uint32_t rtv_descriptor_heap_size;
+    uint32_t dsv_descriptor_heap_size;
+    uint32_t push_constant_size;
+    std::span<D3D12_STATIC_SAMPLER_DESC> static_samplers;
+};
+
+struct D3D12_Context
+{
+    IDXGIFactory7* factory;
+    IDXGIAdapter4* adapter;
+    ID3D12Device10* device;
+    ID3D12CommandQueue* direct_queue;
+    ID3D12CommandQueue* compute_queue;
+    ID3D12CommandQueue* copy_queue;
+    D3D12_Features features;
+    ID3D12DescriptorHeap* resource_descriptor_heap;
+    ID3D12DescriptorHeap* sampler_descriptor_heap;
+    ID3D12DescriptorHeap* rtv_descriptor_heap;
+    ID3D12DescriptorHeap* dsv_descriptor_heap;
+    ID3D12RootSignature* bindless_root_signature;
+};
+
 struct D3D12_Fence : public Fence
 {
     ID3D12Fence1* fence;
@@ -100,7 +168,7 @@ public:
     [[nodiscard]] uint32_t get_uav_from_bindless_index(uint32_t bindless_index) const noexcept;
     [[nodiscard]] const Indirect_Signatures& get_indirect_signatures() const noexcept;
 
-    [[nodiscard]] core::d3d12::D3D12_Context* get_context() noexcept;
+    [[nodiscard]] D3D12_Context* get_context() noexcept;
 
     [[nodiscard]] uint32_t create_descriptor_index_blocking(D3D12_DESCRIPTOR_HEAP_TYPE type) noexcept;
     void release_descriptor_index_blocking(uint32_t index, D3D12_DESCRIPTOR_HEAP_TYPE type) noexcept;
@@ -137,7 +205,7 @@ private:
 private:
     constexpr static std::size_t ARRAY_VECTOR_SIZE = 512;
 
-    core::d3d12::D3D12_Context m_context;
+    D3D12_Context m_context;
     D3D12MA::Allocator* m_allocator;
 
     Descriptor_Increment_Sizes m_descriptor_increment_sizes;
