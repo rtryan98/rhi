09abd32184b7101d7e3bb14983ec7aafc8ea8ac7
Robert Ryan <33040161+rtryan98@users.noreply.github.com>
2026-05-11 10:32:08 +0200

    remove unnecessary translation functions and linked list members

--- src/rhi/rhi/d3d12/d3d12_resource.hpp
+++ src/rhi/rhi/d3d12/d3d12_resource.hpp
@@ -18,25 +18,21 @@ struct D3D12_Buffer : public Buffer
 {
     ID3D12Resource2* resource;
     D3D12MA::Allocation* allocation;
-    D3D12_Buffer_View* buffer_view_linked_list_head;
 };
 
 struct D3D12_Buffer_View : public Buffer_View
 {
-    D3D12_Buffer_View* next_buffer_view;
 };
 
 struct D3D12_Image : public Image
 {
     ID3D12Resource2* resource;
     D3D12MA::Allocation* allocation;
-    D3D12_Image_View* image_view_linked_list_head;
 };
 
 struct D3D12_Image_View : public Image_View
 {
     uint32_t rtv_dsv_index;
-    D3D12_Image_View* next_image_view;
 };
 
 struct D3D12_Sampler : public Sampler
@@ -49,10 +45,6 @@ struct D3D12_Pipeline : public Pipeline
 };
 
 [[nodiscard]] DXGI_FORMAT translate_format(Image_Format format) noexcept;
-[[nodiscard]] D3D12_SRV_DIMENSION translate_view_type_srv(Image_View_Type type) noexcept;
-[[nodiscard]] D3D12_UAV_DIMENSION translate_view_type_uav(Image_View_Type type) noexcept;
-[[nodiscard]] D3D12_RTV_DIMENSION translate_view_type_rtv(Image_View_Type type) noexcept;
-[[nodiscard]] D3D12_DSV_DIMENSION translate_view_type_dsv(Image_View_Type type) noexcept;
 [[nodiscard]] D3D12_FILTER translate_filter(
     Sampler_Filter min, Sampler_Filter mag, Sampler_Filter mip,
     Sampler_Reduction_Type reduction, bool aniso) noexcept;
