1fa29c99043d0fb4567834ad946d5df7046bed91
Robert Ryan <33040161+rtryan98@users.noreply.github.com>
2026-06-12 12:09:02 +0200

    Add missing flag in Vulkan buffer creation for SBT usage

--- src/rhi/rhi/vulkan/vulkan_graphics_device.cpp
+++ src/rhi/rhi/vulkan/vulkan_graphics_device.cpp
@@ -274,7 +274,8 @@ std::expected<Buffer*, Result> Vulkan_Graphics_Device::create_buffer(
             | VK_BUFFER_USAGE_INDEX_BUFFER_BIT
             | VK_BUFFER_USAGE_STORAGE_BUFFER_BIT
             | VK_BUFFER_USAGE_INDIRECT_BUFFER_BIT
-            | VK_BUFFER_USAGE_ACCELERATION_STRUCTURE_BUILD_INPUT_READ_ONLY_BIT_KHR,
+            | VK_BUFFER_USAGE_ACCELERATION_STRUCTURE_BUILD_INPUT_READ_ONLY_BIT_KHR
+            | VK_BUFFER_USAGE_SHADER_BINDING_TABLE_BIT_KHR,
         .sharingMode = VK_SHARING_MODE_CONCURRENT,
         .queueFamilyIndexCount = static_cast<uint32_t>(queue_families.size()),
         .pQueueFamilyIndices = queue_families.data()
